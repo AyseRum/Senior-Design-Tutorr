3e3dfeb64a7097e368ebf447a957797c2087d1d1
Ayse Rumeysa Ergin <ayserumeysaergin@gmail.com>
2019-04-15 21:33:22 -0500

    changes

---
+++ init-firebase.js
@@ -0,0 +1,12 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAEXCf44KMPZc-CKzT7q3236o-gHOjn1cE",
+	authDomain: "senior-design-tutorr.firebaseapp.com",
+	databaseURL: "https://senior-design-tutorr.firebaseio.com",
+	projectId: "senior-design-tutorr",
+	storageBucket: "senior-design-tutorr.appspot.com",
+	messagingSenderId: "24393945422"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
